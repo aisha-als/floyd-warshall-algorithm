acd2c7cdd06ab4d4d599dcc77e7acb36fa6779c5
Aisha Al-Semaiti <41858301+aisha-als@users.noreply.github.com>
2024-02-20 12:06:26 +0700

    Adding docstrings to unit tests.

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -14,18 +14,23 @@ class TestDatasets(unittest.TestCase):
         self.assertEqual(floyd_recursive(data_tests.original_graph), data_tests.original_expected)
 
     def test_a(self):
+        """test_a - 5x5 adjacency matrix. """
         self.assertEqual(floyd_recursive(data_tests.test_a), data_tests.a_expected)
 
     def test_b(self):
+        """test_b - 8x8 adjacency matrix. """
         self.assertEqual(floyd_recursive(data_tests.test_b), data_tests.b_expected)
 
     def test_c(self):
+        """test_c - float values. """
         self.assertEqual(floyd_recursive(data_tests.test_c), data_tests.c_expected)
 
     def test_d(self):
+        """test_d - non-square adjacency matrix, expected error. """
         self.assertRaises(IndexError, floyd_recursive, data_tests.test_d)
 
     def test_e(self):
+        """test_e - 16x16 adjacency matrix. """
         self.assertEqual(floyd_recursive(data_tests.test_e), data_tests.e_expected)
 
 
